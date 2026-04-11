596629a80702364431177e2cd0b6e3ce20877269
Alina Hashmi <heybbyali@gmail.com>
2026-04-12 03:23:14 +0530

    Implement integer comparison with if-else
    Add a program to compare two integers and print the result.

---
+++ Week1-C/files/ifelse.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+{
+
+int main(void)
+int x = get_int("enter x is?");
+int y = get_int("enter y is?);
+
+if (x < y)
+{
+    printf("x is less than y\n");
+}
+else
+{
+    printf("x is not less than y\n")
+}
+}
