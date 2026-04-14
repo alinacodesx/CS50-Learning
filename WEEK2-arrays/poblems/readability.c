8e35e0c401a0840fe73d4ecfeab095f17c8fb564
Alina Hashmi <heybbyali@gmail.com>
2026-04-14 18:11:57 +0530

    Refactor readability.c for clarity and consistency
    Reorganize includes and fix formatting issues.

--- WEEK2-arrays/poblems/readability.c
+++ WEEK2-arrays/poblems/readability.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string text = get_string("Text: ");
     int letters = 0, words = 1, sentences = 0;
-    for (int i = 0, n = strlen(text); i <n; i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isalpha(text[i]))
-        letters++;
+            letters++;
         else if (text[i] == ' ')
-        words++;
-        else if (text[i] == '.' || text[i] == '!'|| text[i] == '?')
-        sentences++;
+            words++;
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            sentences++;
     }
 
     float L = (float) letters / words * 100;
@@ -24,8 +24,11 @@ int main(void)
     int index = round(0.0588 * L - 0.296 * S - 15.8);
 
     if (index < 1)
-    printf("Before Grade 1\n");
+        printf("Before Grade 1\n");
     else if (index >= 16)
-    printf("Grade %d\n", index);
+        printf("Grade 16+\n");
+    else
+        printf("Grade %d\n", index);
+
     return 0;
 }
